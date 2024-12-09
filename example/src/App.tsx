4a14c0eb0cd8f2d5ad066bb47fbe00d7846e7613
Fabbro <fabbro@infograb.net>
2024-12-09 23:00:26 +0900

    feat: 예제 프로젝트 추가

---
+++ example/src/App.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { PopChildren } from "../../src";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>React Pop Effect 테스트</h1>
+
+      <div className="card">
+        <div className="example-row">
+          <h3>기본 효과 (날아가기)</h3>
+          <PopChildren>
+            <button className="pop-button">
+              클릭하거나 누르고 있어보세요!
+            </button>
+          </PopChildren>
+        </div>
+
+        <div className="example-row">
+          <h3>바닥에 쌓이는 효과</h3>
+          <PopChildren stackOnGround maxStackedItems={30} spawnInterval={100}>
+            <div className="box">여기도 클릭해보세요!</div>
+          </PopChildren>
+        </div>
+
+        <div className="example-row">
+          <h3>빠른 생성 효과</h3>
+          <PopChildren spawnInterval={30}>
+            <div className="circle">빠르게!</div>
+          </PopChildren>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
