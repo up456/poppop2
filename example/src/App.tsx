8d9d05b5f1320e9f4e1d044fad8aa37b4332a4b4
Fabbro <fabbro@infograb.net>
2024-12-18 23:44:40 +0900

    design: 예제 파일 제목 스타일 수정

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import { PopChildren, AnimationEnum } from "poppop2"
+import { PopChildren } from "poppop2"
 import "./App.css"
 
 export default function Home() {
   return (
     <div className="app">
-      <h1>React Pop Effect 테스트</h1>
+      <h1 className="title">React Pop Effect 테스트</h1>
 
       <div className="container">
         <div className="example-box">
@@ -25,7 +25,7 @@ export default function Home() {
 
         <div className="example-box">
           <h3 className="margin-bottom-3"> 확산 + 쌓임</h3>
-          <PopChildren maxStackedItems={50} animationType={AnimationEnum.SPREAD} isStackable range={500}>
+          <PopChildren maxStackedItems={50} animationType="spread" isStackable range={500}>
             <div className="heart">
               <span className="heart-text">사랑해!</span>
             </div>
@@ -33,13 +33,13 @@ export default function Home() {
         </div>
         <div className="example-box">
           <h3>빠른 + 확산 효과</h3>
-          <PopChildren spawnInterval={20} maxStackedItems={50} animationType={AnimationEnum.SPREAD}>
+          <PopChildren spawnInterval={20} maxStackedItems={50} animationType="spread">
             <div className="circle">빠르다</div>
           </PopChildren>
         </div>
         <div className="example-box">
           <h3>svg</h3>
-          <PopChildren spawnInterval={20} maxStackedItems={50} animationType={AnimationEnum.SPREAD}>
+          <PopChildren spawnInterval={20} maxStackedItems={50} animationType="spread">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="-2 -2 28 28"
@@ -57,7 +57,7 @@ export default function Home() {
         </div>
         <div className="example-box">
           <h3>image</h3>
-          <PopChildren spawnInterval={20} maxStackedItems={50} animationType={AnimationEnum.SPREAD}>
+          <PopChildren spawnInterval={20} maxStackedItems={50} animationType="spread">
             <img
               className="circle"
               src="https://images.unsplash.com/photo-1507146426996-ef05306b995a?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
